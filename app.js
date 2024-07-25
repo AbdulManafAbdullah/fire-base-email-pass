de1c6f33bcadf76b6395a92af22bf8ac617bda47
AbdulManafAbdullah <mannanzuni91@gmail.com>
2024-07-25 13:01:09 +0500

    changes

--- app.js
+++ app.js
@@ -14,15 +14,7 @@ import {
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDosAyqDTXIRpH8REEkFbeqf0YELhe0yN4",
-  authDomain: "my-first-project-6783f.firebaseapp.com",
-  projectId: "my-first-project-6783f",
-  storageBucket: "my-first-project-6783f.appspot.com",
-  messagingSenderId: "814886795960",
-  appId: "1:814886795960:web:80d61476db23c9bb98124d",
-  measurementId: "G-3SJFWH6STS",
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
